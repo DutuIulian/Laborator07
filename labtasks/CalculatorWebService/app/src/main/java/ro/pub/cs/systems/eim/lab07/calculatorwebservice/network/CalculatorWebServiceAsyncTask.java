a84b051affb3556c2b2cfee0b750efbaff0b6781
Dutu Iulian <iulian.dutu@wirecard.com>
2021-04-26 11:38:04 +0300

    Laborator 07

--- labtasks/CalculatorWebService/app/src/main/java/ro/pub/cs/systems/eim/lab07/calculatorwebservice/network/CalculatorWebServiceAsyncTask.java
+++ labtasks/CalculatorWebService/app/src/main/java/ro/pub/cs/systems/eim/lab07/calculatorwebservice/network/CalculatorWebServiceAsyncTask.java
@@ -3,6 +3,24 @@ package ro.pub.cs.systems.eim.lab07.calculatorwebservice.network;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.ClientProtocolException;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import cz.msebera.android.httpclient.protocol.HTTP;
+import ro.pub.cs.systems.eim.lab07.calculatorwebservice.general.Constants;
+
 public class CalculatorWebServiceAsyncTask extends AsyncTask<String, Void, String> {
 
     private TextView resultTextView;
@@ -20,22 +38,56 @@ public class CalculatorWebServiceAsyncTask extends AsyncTask<String, Void, Strin
 
         // TODO exercise 4
         // signal missing values through error messages
+        if(operator1 == null || operator1.isEmpty() || operator2 == null || operator2.isEmpty()) {
+            return Constants.ERROR_MESSAGE_EMPTY;
+        }
 
         // create an instance of a HttpClient object
+        HttpClient httpClient = new DefaultHttpClient();
 
         // get method used for sending request from methodsSpinner
-
-        // 1. GET
-        // a) build the URL into a HttpGet object (append the operators / operations to the Internet address)
-        // b) create an instance of a ResultHandler object
-        // c) execute the request, thus generating the result
-
-        // 2. POST
-        // a) build the URL into a HttpPost object
-        // b) create a list of NameValuePair objects containing the attributes and their values (operators, operation)
-        // c) create an instance of a UrlEncodedFormEntity object using the list and UTF-8 encoding and attach it to the post request
-        // d) create an instance of a ResultHandler object
-        // e) execute the request, thus generating the result
+        if(method == Constants.GET_OPERATION) {
+            // 1. GET
+            // a) build the URL into a HttpGet object (append the operators / operations to the Internet address)
+            HttpGet httpGet = new HttpGet(Constants.GET_WEB_SERVICE_ADDRESS
+                    + "?" + Constants.OPERATION_ATTRIBUTE + "=" + operation
+                    + "&" + Constants.OPERATOR1_ATTRIBUTE + "=" + operator1
+                    + "&" + Constants.OPERATOR2_ATTRIBUTE + "=" + operator2);
+            // b) create an instance of a ResultHandler object
+            ResponseHandler<String> responseHandlerGet = new BasicResponseHandler();
+            // c) execute the request, thus generating the result
+            try {
+                return httpClient.execute(httpGet, responseHandlerGet);
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } if(method == Constants.POST_OPERATION) {
+            // 2. POST
+            // a) build the URL into a HttpPost object
+            HttpPost httpPost = new HttpPost(Constants.POST_WEB_SERVICE_ADDRESS);
+            // b) create a list of NameValuePair objects containing the attributes and their values (operators, operation)
+            List<NameValuePair> parameters = new ArrayList<>();
+            parameters.add(new BasicNameValuePair(Constants.OPERATION_ATTRIBUTE, operation));
+            parameters.add(new BasicNameValuePair(Constants.OPERATOR1_ATTRIBUTE, operator1));
+            parameters.add(new BasicNameValuePair(Constants.OPERATOR2_ATTRIBUTE, operator2));
+            try {
+                // c) create an instance of a UrlEncodedFormEntity object using the list and UTF-8 encoding and attach it to the post request
+                UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(parameters, HTTP.UTF_8);
+                httpPost.setEntity(urlEncodedFormEntity);
+                // d) create an instance of a ResultHandler object
+                ResponseHandler<String> responseHandlerPost = new BasicResponseHandler();
+                // e) execute the request, thus generating the result
+                return httpClient.execute(httpPost, responseHandlerPost);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         return null;
     }
@@ -43,6 +95,7 @@ public class CalculatorWebServiceAsyncTask extends AsyncTask<String, Void, Strin
     @Override
     protected void onPostExecute(String result) {
         // display the result in resultTextView
+        resultTextView.setText(result);
     }
 
 }
